fc3bf35234a72446694f229fe59dbaa7629c6f2e
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-07-29 10:59:06 +0100

    image styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,33 +1,33 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import styles from '../../styles/Hero.module.css'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import styles from "../../styles/Hero.module.css";
 
-function Hero({title}) {
+function Hero({ title }) {
   return (
     <div className={styles.hero}>
-        <Image
-          src={"/sklogo.png"}
-          width={700}
-          height={350}
-          alt="sklogo"
-        />
+      <Image
+        src={"/sklogo.png"}
+        width={700}
+        height={400}
+        alt="sklogo"
+        className={styles.bigLogo}
+      />
 
-        <div className={styles.title}>
-          <h1>{title}</h1>
+      <div className={styles.title}>
+        <h1>{title}</h1>
 
-          <div className={styles.buttons}>
-            <Link href={"#contact"}>
-              <button>Contact</button>
-            </Link>
-            <Link href={"https://www.beatstars.com/realskproductions"}>
-              <button>Buy now</button>
-            </Link>
-          </div>
+        <div className={styles.buttons}>
+          <Link href={"#contact"}>
+            <button>Contact</button>
+          </Link>
+          <Link href={"https://www.beatstars.com/realskproductions"}>
+            <button>Buy now</button>
+          </Link>
         </div>
       </div>
-
-  )
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
