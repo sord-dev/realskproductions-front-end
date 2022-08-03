134e252e928b3c7d95f501f50e4d90961671e10b
sord-dev <75338985+sord-dev@users.noreply.github.com>
2022-08-03 12:48:12 +0100

    error resolved

--- next.config.js
+++ next.config.js
@@ -4,17 +4,9 @@ require('dotenv').config()
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-<<<<<<< HEAD
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-};
-=======
   env: {
     INSTAGRAM_USER_TOKEN: process.env.INSTAGRAM_USER_TOKEN
   }
 }
->>>>>>> working
 
 module.exports = nextConfig;
